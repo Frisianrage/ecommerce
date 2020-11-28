2bc3a47536cd2b2bae3919a270ab55a347083ce0
Frisianrage <65295703+Frisianrage@users.noreply.github.com>
2020-11-28 20:01:18 +0100

    authUser backend finished

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,24 +1,10 @@
 const express = require('express')
-const Product = require('../models/productModel')
-const asyncHandler = require('express-async-handler')
+const { getProducts, getProductById } = require('../controller/productController.js')
 
 const router = express.Router()
 
-router.get('/', asyncHandler(async(req, res) => {
-    const products = await Product.find({})
-    res.json(products)
-}))
+router.route('/').get(getProducts)
 
-router.get('/:id', asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id)
-
-    if(product) {
-        res.json(product)
-    } else {
-        res.status(404)
-        throw new Error('Product not found!')
-    }
-    
-}))
+router.route('/:id').get(getProductById)
 
 module.exports = router
--- backend/server.js
+++ backend/server.js
@@ -3,19 +3,21 @@ const dotenv = require('dotenv')
 const connectDB = require('./config/db')
 const colors = require('colors')
 const productRoutes = require('./routes/productRoutes')
+const userRoutes = require('./routes/userRoutes')
 const {notFound, errorHandler} = require('./middleware/errorMiddleware.js')
 
 dotenv.config()
-const app = express();
 connectDB()
 
-const PORT = process.env.PORT || 5000
 
-app.get('/', (req, res) => {
-    res.send('APP is running')
-})
+const app = express();
+const PORT = process.env.PORT || 5000
+app.use(express.json())
+app.get('/', (req, res) => {res.send('APP is running')})
 
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
+
 
 app.use(notFound)
 app.use(errorHandler)
