5ac913508ce53c05b506b9b6d41bd25c46e6b283
Frisianrage <65295703+Frisianrage@users.noreply.github.com>
2021-02-02 17:32:15 -0800

    product carousel added

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,13 +1,16 @@
 const express = require('express')
-const { getProducts, getProductById, deleteProduct, createProduct, updateProduct, createProductReview } = require('../controller/productController.js')
+const { getProducts, getProductById, deleteProduct, createProduct, updateProduct, createProductReview, getTopProducts } = require('../controller/productController.js')
 const {protect, admin} = require('../middleware/authMiddleware')
 
 const router = express.Router()
 
 router.route('/').get(getProducts).post(protect, admin, createProduct)
+router.get('/top', getTopProducts)
 
 router.route('/:id').get(getProductById).delete(protect, admin, deleteProduct).put(protect, admin, updateProduct)
 
 router.route('/:id/reviews').post(protect, createProductReview)
+
+
 
 module.exports = router
