b719198876643191cae28ff40cb01e34d9f03e24
Frisianrage <65295703+Frisianrage@users.noreply.github.com>
2021-02-02 18:22:44 -0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -17,9 +17,10 @@ const app = express();
 const PORT = process.env.PORT || 5000
 
 app.use(express.json())
-app.use(morgan('dev'))
 
-app.get('/', (req, res) => {res.send('APP is running')})
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+  }
 
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
@@ -30,6 +31,18 @@ app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_I
 
 const dirname = path.resolve()
 app.use('/uploads', express.static(path.join(dirname, '/uploads')))
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+  
+    app.get('*', (req, res) =>
+      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    )
+  } else {
+    app.get('/', (req, res) => {
+      res.send('API is running....')
+    })
+  }
 
 app.use(notFound)
 app.use(errorHandler)
